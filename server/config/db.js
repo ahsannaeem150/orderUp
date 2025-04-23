dfa2a5f81b49ceea65144da91ca8ae806f9d2f94
Ahsan Naeem <ahsannaeem150@gmail.com>
2025-04-24 01:40:26 +0500

    script change for deployement in server package.json

--- server/config/db.js
+++ server/config/db.js
@@ -8,11 +8,22 @@ import mongoose from "mongoose";
 //     console.log(`Error in DB error ${error}`.bgCyan.white);
 //   }
 // };
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.LOCAL_MONGO_URL, {
+//       replicaSet: "rs0",
+//     });
+//     console.log(`Connected to DB ${mongoose.connection.host}`.bgCyan.white);
+//   } catch (error) {
+//     console.log(`DB Error: ${error}`.bgRed.white);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.LOCAL_MONGO_URL, {
-      replicaSet: "rs0",
-    });
+    await mongoose.connect(process.env.GLOBAL_ATLAS_MONGO_URL);
     console.log(`Connected to DB ${mongoose.connection.host}`.bgCyan.white);
   } catch (error) {
     console.log(`DB Error: ${error}`.bgRed.white);
